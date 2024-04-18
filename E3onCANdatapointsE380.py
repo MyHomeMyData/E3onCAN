e0dd86a1cf93a93562e18936fb5b8507823afa79
MyHomeMyData <juergen.bonfert@gmail,com>
2024-04-18 11:27:07 +0200

    Updated datapoints of E380 to version 20240418

--- E3onCANdatapointsE380.py
+++ E3onCANdatapointsE380.py
@@ -24,16 +24,14 @@ dataIdentifiersE380 = {
     0x251: O3EComplexType(8, "GridActivePower", [O3EInt16(2, "L1", scale=1.0, signed=True), O3EInt16(2, "L2", scale=1.0, signed=True), O3EInt16(2, "L3", scale=1.0, signed=True), O3EInt16(2, "Total", scale=1.0, signed=True)]),
     0x252: O3EComplexType(8, "GridReactivePower", [O3EInt16(2, "L1", scale=1.0, signed=True), O3EInt16(2, "L2", scale=1.0, signed=True), O3EInt16(2, "L3", scale=1.0, signed=True), O3EInt16(2, "Total", scale=1.0, signed=True)]),
     0x253: O3EComplexType(8, "GridReactivePower", [O3EInt16(2, "L1", scale=1.0, signed=True), O3EInt16(2, "L2", scale=1.0, signed=True), O3EInt16(2, "L3", scale=1.0, signed=True), O3EInt16(2, "Total", scale=1.0, signed=True)]),
-    0x254: O3EComplexType(8, "GridCurrent", [O3EInt16(2, "L1", scale=1.0, signed=True), O3EInt16(2, "L2", scale=1.0, signed=True), O3EInt16(2, "L3", scale=1.0, signed=True), O3EcosPhi(2, "cosPhi", scale=100.0, signed=True)]),
-#    0x254: O3EComplexType(8, "GridCurrent", [O3EFloat16(2, "L1", scale=1.0), O3EFloat16(2, "L2", scale=1.0), O3EFloat16(2, "L3", scale=1.0), O3EcosPhi(2, "cosPhi", scale=100.0, signed=True)]),
-    0x255: O3EComplexType(8, "GridCurrent", [O3EInt16(2, "L1", scale=1.0, signed=True), O3EInt16(2, "L2", scale=1.0, signed=True), O3EInt16(2, "L3", scale=1.0, signed=True), O3EcosPhi(2, "cosPhi", scale=100.0, signed=True)]),
-#    0x255: O3EComplexType(8, "GridCurrent", [O3EFloat16(2, "L1", scale=1.0), O3EFloat16(2, "L2", scale=1.0), O3EFloat16(2, "L3", scale=1.0), O3EcosPhi(2, "cosPhi", scale=100.0, signed=True)]),
+    0x254: O3EComplexType(8, "GridCurrent", [O3EInt16(2, "Absolute L1", scale=1.0, signed=True), O3EInt16(2, "Absolute L2", scale=1.0, signed=True), O3EInt16(2, "Absolute L3", scale=1.0, signed=True), O3EcosPhi(2, "cosPhi", scale=100.0, signed=True)]),
+    0x255: O3EComplexType(8, "GridCurrent", [O3EInt16(2, "Absolute L1", scale=1.0, signed=True), O3EInt16(2, "Absolute L2", scale=1.0, signed=True), O3EInt16(2, "Absolute L3", scale=1.0, signed=True), O3EcosPhi(2, "cosPhi", scale=100.0, signed=True)]),
     0x256: O3EComplexType(8, "GridVoltage", [O3EInt16(2, "L1", scale=1.0, signed=False), O3EInt16(2, "L2", scale=1.0, signed=False), O3EInt16(2, "L3", scale=1.0, signed=False), O3EInt16(2, "Frequency", scale=100.0, signed=False)]),
     0x257: O3EComplexType(8, "GridVoltage", [O3EInt16(2, "L1", scale=1.0, signed=False), O3EInt16(2, "L2", scale=1.0, signed=False), O3EInt16(2, "L3", scale=1.0, signed=False), O3EInt16(2, "Frequency", scale=100.0, signed=False)]),
     0x258: O3EComplexType(8, "GridEnergy", [O3EFloat32(4, "ImportCumulated", scale=1000.0), O3EFloat32(4, "ExportCumulated", scale=1000.0)]),  # Cumulated import and export (kWh)
     0x259: O3EComplexType(8, "GridEnergy", [O3EFloat32(4, "ImportCumulated", scale=1000.0), O3EFloat32(4, "ExportCumulated", scale=1000.0)]),  # Cumulated import and export (kWh)
     0x25A: O3EComplexType(8, "GridPower", [O3EInt32(4, "ActivePower", scale=10.0, signed=True), O3EInt32(4, "ReactivePower", scale=10.0, signed=True)]),
     0x25B: O3EComplexType(8, "GridPower", [O3EInt32(4, "ActivePower", scale=10.0, signed=True), O3EInt32(4, "ReactivePower", scale=10.0, signed=True)]),
-    0x25C: O3EComplexType(4, "GridEnergy", [O3EInt32(4, "ImportCumulated", scale=100, signed=False)]),   # Cumulated import (kWh)
-    0x25D: O3EComplexType(4, "GridEnergy", [O3EInt32(4, "ImportCumulated", scale=100, signed=False)])    # Cumulated import (kWh)
+    0x25C: O3EComplexType(8, "GridEnergy", [O3EInt32(4, "ImportCumulated", scale=100, signed=False), O3EInt32(4, "Unknown", scale=1, signed=False)]),   # Cumulated import (kWh)
+    0x25D: O3EComplexType(8, "GridEnergy", [O3EInt32(4, "ImportCumulated", scale=100, signed=False), O3EInt32(4, "Unknown", scale=1, signed=False)])    # Cumulated import (kWh)
 }
